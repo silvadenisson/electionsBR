4b34a37dd9d28d9d014a94832bfc3c9ce449f066
Fernando Meireles <fernando.meireles@ufrgs.br>
2016-10-09 00:36:22 -0300

    New commit

--- R/vote_mun_zone_local.R
+++ R/vote_mun_zone_local.R
@@ -41,6 +41,7 @@
 #'   \item NOME_COLIGACAO: COalition name.
 #'   \item COMPOSICAO_LEGENDA: Coalition's composition.
 #'   \item TOTAL_VOTOS: Total of votes.
+#'   \item TRANSITO: Electoral result outside the candidates' district? (N for no).
 #'  }
 #'
 #' @import utils
@@ -73,6 +74,7 @@ vote_mun_zone_local <- function(year, ascii = FALSE){
   unlink(as.character(year), recursive = T)
 
   # Change variable names
+  if(year <= 2012){
     names(banco) <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO", "DESCRICAO_ELEICAO",
                       "SIGLA_UF", "SIGLA_UE", "CODIGO_MUNICIPIO", "NOME_MUNICIPIO", "NUMERO_ZONA",
                       "CODIGO_CARGO", "NUMERO_CAND", "SQ_CANDIDATO", "NOME_CANDIDATO", "NOME_URNA_CANDIDATO",
@@ -80,6 +82,18 @@ vote_mun_zone_local <- function(year, ascii = FALSE){
                       "DESC_SIT_CANDIDATO", "CODIGO_SIT_CAND_TOT", "DESC_SIT_CAND_TOT", "NUMERO_PARTIDO",
                       "SIGLA_PARTIDO", "NOME_PARTIDO", "SEQUENCIAL_LEGENDA", "NOME_COLIGACAO", "COMPOSICAO_LEGENDA",
                       "TOTAL_VOTOS")
+  
+  } else {
+      
+    names(banco) <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", "NUM_TURNO", "DESCRICAO_ELEICAO",
+                      "SIGLA_UF", "SIGLA_UE", "CODIGO_MUNICIPIO", "NOME_MUNICIPIO", "NUMERO_ZONA",
+                      "CODIGO_CARGO", "NUMERO_CAND", "SQ_CANDIDATO", "NOME_CANDIDATO", "NOME_URNA_CANDIDATO",
+                      "DESCRICAO_CARGO", "COD_SIT_CAND_SUPERIOR", "DESC_SIT_CAND_SUPERIOR", "CODIGO_SIT_CANDIDATO",
+                      "DESC_SIT_CANDIDATO", "CODIGO_SIT_CAND_TOT", "DESC_SIT_CAND_TOT", "NUMERO_PARTIDO",
+                      "SIGLA_PARTIDO", "NOME_PARTIDO", "SEQUENCIAL_LEGENDA", "NOME_COLIGACAO", "COMPOSICAO_LEGENDA",
+                      "TOTAL_VOTOS", "TRANSITO")
+    
+    }
 
   # Change to ascii
   if(ascii == T) banco <- to_ascii(banco)
