1e2496bd82cae8224ad977b9c3f1bb6bb655e60e
Fernando Meireles <fernando.meireles@ufrgs.br>
2016-11-24 18:30:16 -0200

    New commit

---
+++ R/voter_profile.R
@@ -0,0 +1,72 @@
+#' Download data on the voters' profile
+#'
+#' \code{voter_profile()} downloads and cleans data on the voters' profile aggregated by state, city and electoral zone.
+#' The function returns a \code{data.frame} where each observation corresponds to a voter profile type.
+#'
+#' @param year Election year (\code{integer}). For this function, the following years are available: 1994, 1996, 1998,
+#' 2000, 2002, 2004, 2006, 2008, 2010, 2012, 2014 and 2016.
+#' 
+#' @param ascii (\code{logical}). Should the text be transformed from Latin-1 to ASCII format?
+#'
+#' @param encoding Data original encoding (defaults to 'windows-1252'). This can be changed to avoid errors
+#' when \code{ascii = TRUE}.
+#'
+#' @return \code{voter_profile()} returns a \code{data.frame} with the following variables:
+#'
+#' \itemize{
+#'   \item PERIODO: Election year.
+#'   \item UF: Units of the Federation's acronym in which occurred the election.
+#'   \item MUNICIPIO: Municipality name.
+#'   \item COD_MUNICIPIO_TSE: Municipal's Supreme Electoral Court code (number).
+#'   \item NR_ZONA: Electoral zone's Supreme Electoral Court code (number).
+#'   \item SEXO: Voters' sex.
+#'   \item FAIXA_ETARIA: Voters' age group.
+#'   \item GRAU_DE_ESCOLARIDADE: Voters' education degree.
+#'   \item QTD_ELEITORES_NO_PERFIL: Absolute number of voters.
+#' }
+#'
+#' @import utils
+#' @importFrom magrittr "%>%"
+#' @export
+#' @examples
+#' \dontrun{
+#' df <- voter_profile(2002)
+#' }
+
+voter_profile <- function(year, ascii = FALSE, encoding = "windows-1252"){
+  
+  
+  # Inputs
+  if(!year %in% seq(1994, 2016, by = 2)) stop("Invalid 'year'. Please check the documentation and try again.")
+  test_encoding(encoding)
+  
+  # Download data
+  dados <- tempfile()
+  local <- tempdir()
+  sprintf("http://agencia.tse.jus.br/estatistica/sead/odsele/perfil_eleitorado/perfil_eleitorado_%s.zip", year) %>%
+    download.file(dados)
+  unzip(dados, exdir = local)
+  unlink(dados)
+  
+  # Join data
+  message("Processing the data...")
+  
+  orig <- getwd()
+  setwd(local)
+  banco <- Sys.glob("*.txt") %>%
+    read.table(header = F, sep = ";", stringsAsFactors = F, fill = T, fileEncoding = "windows-1252")
+  
+  setwd(orig)
+  unlink(local, recursive = T)
+  
+  # Change variable names
+  names(banco) <- c("PERIODO", "UF", "MUNICIPIO", "COD_MUNICIPIO_TSE", "NR_ZONA",
+                    "SEXO", "FAIXA_ETARIA", "GRAU_DE_ESCOLARIDADE", "QTD_ELEITORES_NO_PERFIL")
+  
+  # Change to ascii
+  if(ascii) banco <- to_ascii(banco, encoding)
+  
+  message("Done.")
+  banco
+}
+
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ electionsBR
 [![Travis-CI Build Status](https://travis-ci.org/silvadenisson/electionsBR.svg?branch=master)](https://travis-ci.org/silvadenisson/electionsBR) [![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/silvadenisson/electionsBR?branch=master&svg=true)](https://ci.appveyor.com/project/silvadenisson/electionsBR) [![Package-License](https://img.shields.io/badge/license-GPL%20%28%3E=%202%29-brightgreen.svg?style=flat)](http://www.gnu.org/licenses/gpl-2.0.html)
 
 `electionsBR` contains a set of functions that automatically downloads and aggregates
-election data from Brazil (1996-2014), directly from the [Brazilian Superior Electoral Court website (TSE)](http://www.tse.jus.br/eleicoes/estatisticas/repositorio-de-dados-eleitorais).
+election data from Brazil (1996-2016), directly from the [Brazilian Superior Electoral Court website (TSE)](http://www.tse.jus.br/eleicoes/estatisticas/repositorio-de-dados-eleitorais).
 Among others, there are data available on local and federal elections for all positions (city councillor, mayor, state deputy, federal deputy, governor, and president) disaggregated by state of the Federation, city, zone, and polling stations.
 
 ### Installation
@@ -37,6 +37,7 @@ devtools::install_github("silvadenisson/electionsBR")
 * vote_mun_zone_fed()
 * vote_mun_zone_local()
 * voter_affiliation()
+* voter_profile()
 
 To download all the data from a given election, only the `year` argument must be passed to the function call:
 
