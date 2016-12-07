857e783b18a10d0039a5f210fcb43b5192adb12e
Denisson Silva <denisson@denissonsilva.com>
2016-12-07 11:57:36 -0200

    Update voter_profile.R

--- R/voter_profile.R
+++ R/voter_profile.R
@@ -56,9 +56,8 @@ voter_profile <- function(year, ascii = FALSE, encoding = "windows-1252"){
   banco <- Sys.glob("*.txt") %>%
     read.table(header = F, sep = ";", stringsAsFactors = F, fill = T, fileEncoding =  'windows-1252')
   
+  file.remove(dir(local, full.names=TRUE))
   setwd(orig)
-  
-file.remove(dir(local, full.names=TRUE))
     
   # Change variable names
   names(banco) <- c("PERIODO", "UF", "MUNICIPIO", "COD_MUNICIPIO_TSE", "NR_ZONA",
