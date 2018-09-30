f3370f0b678e2b61e4acf2c1700fb3e50229322d
silvadenisson <denisson@denissonsilva.com>
2018-09-30 18:23:12 -0300

    Readrds
    Readrds

--- R/candidate_fed_rda.R
+++ R/candidate_fed_rda.R
@@ -2,14 +2,16 @@
 #'
 #' @param year Election year. For this function, onlye the years of 1996, 2000, 2004, 2008, 2012 and 2016
 #' are available.
+#' atualizacao do arquivo a cada 6 meses
 #' @export
 #' @examples
 #' \dontrun{
-#' candidate_fed_rda(2000)
+#' candidate_fed_rda(2018)
 #' }
 
 candidate_fed_rda <- function(year){
-  load(url(paste0("http://electionsbr.com/api_rda/candidate_fed_", year, ".Rda")))
+  data <- readRDS(url(paste0("http://electionsbr.com/api_rda/candidate_fed_", year, ".Rda")))
+  return(data)
 }
 
 
